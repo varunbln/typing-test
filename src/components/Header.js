2ab63129a901abe40e845e50bfe9fec489f5dff1
varun-balani <varunprahladb@gmail.com>
2023-08-31 19:11:28 +0530

    remove stats

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,6 @@ export function Header() {
         <header className="pt-3 pb-3 sm:pt-0 sm:pb-0 row-span-1 col-span-3 text-lg flex flex-row justify-between align-middle border-b-2 border-gray-800">
             <button id="home-button" className="ml-4 sm:ml-6 sm:pl-8 sm:pr-8 text-gray-200 font-semibold">TypingTest</button>
             <div id="login-needed-buttons" className="flex flex-row justify-between">
-                <button className="hover:text-gray-200 font-semibold">Stats</button>
                 {userId === null ? <LoginButton /> : <ProfileButton />}
             </div>
         </header>
